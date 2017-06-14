d4967a925a29cfd56b40cb86186ef5f1e1a9d18e
Jeehoon Yoo <jeehoon.yoo@ridi.com>
2017-06-14 14:37:17 +0900

    Do not record the results (input text/output results).
    Do not record the results (input text/output results).

--- log.py
+++ log.py
@@ -9,17 +9,7 @@ class Log(object):
     
     @classmethod
     def log_parsed_bible_word(cls, input_text, bible_word_list):
-        curr_time = str(datetime.now())
-        Log.write_file(cls.RECORD_DIR + curr_time + cls.INPUT_FILE, input_text)
-
-        output_content = u''
-        for bible_word in bible_word_list:
-            if len(output_content) <= 0:
-                output_content = bible_word
-            else:
-                output_content += u'\n' + bible_word
-
-        Log.write_file(cls.RECORD_DIR + curr_time + cls.OUTPUT_FILE, output_content)
+        pass
         
     @staticmethod
     def write_file(path, content):
