d5a7f98e344fd7f3e72fa0592fa602d21aaf542f
Jeehoon Yoo <jihungen@gmail.com>
2017-10-08 13:04:57 +0000

    WIP) Add the feature that downloads pptx file.

---
+++ ppt.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+def ppt_content(book_chapter_verse, version_list, text):
+    return {
+        'book_chapter_verse': book_chapter_verse,
+        'version_list': version_list,
+        'text': text
+    }
